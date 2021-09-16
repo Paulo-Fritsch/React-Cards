54a1af6f81c5e8dd59298b30e745921118bdc1d6
Paulo Cezar Fritsch Júnior <paulo.fritsch2001@gmail.com>
2021-09-15 22:09:24 -0300

    Atualizando o State

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,28 +1,51 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-  handleMudancaTitulo (evento){
-    console.log(evento.target.value);
+  
+  constructor(props){
+    super(props);
+    this.titulo = "";
+    this.texto = "";
   }
-    render() {
-        return (
-        <form className="form-cadastro ">
-            <input
-            type="text"
-            placeholder="Título"
-            className="form-cadastro_input"
-            onChange={this.handleMudancaTitulo}
-            />
-            <textarea
-            rows={15}
-            placeholder="Escreva sua nota..."
-            className="form-cadastro_input"
-            />
-            <button className="form-cadastro_input form-cadastro_submit">
-            Criar Nota
-            </button>
-        </form>
-        );
+
+  _handleMudancaTitulo(evento){
+    this.titulo = evento.target.value
+    evento.stopPropagation();
+  }
+
+  _handleMudancaTexto(evento){
+    this.texto = evento.target.value
+    evento.stopPropagation();
+  }
+
+  _criarcard(evento){
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarcard(this.titulo, this.texto);
+    
+  }
+
+  render() {
+    return (
+      <form className="form-cadastro "
+      onSubmit={this._criarcard.bind(this)}>
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange = {this._handleMudancaTitulo.bind(this)}
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+          onChange = {this._handleMudancaTexto.bind(this)}
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
+      </form>
+    );
   }
 }
 
